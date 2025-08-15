4ebaa29656be43def4eb06c0997f149858d5454c
hakanyalniz <hakan_yalniz@live.com>
2025-08-15 17:32:33 +0300

    Set up some basic code to get an epub and strip it of the tags.

--- aiReviewEdit.py
+++ aiReviewEdit.py
@@ -1,2 +1,26 @@
 from ebooklib import epub
-book = epub.read_epub('novel.epub')
+from bs4 import BeautifulSoup
+import ebooklib
+
+BOOK = epub.read_epub("./testNovels/MachineEnglish.epub")
+STRIPPED_BOOK = BOOK
+
+
+# Function to remove tags
+def remove_tags(html):
+    # parse html content
+    soup = BeautifulSoup(html, "html.parser")
+
+    for data in soup(["style", "script"]):
+        # Remove tags
+        data.decompose()
+
+    # return data by retrieving the tag content
+    return " ".join(soup.stripped_strings)
+
+
+for item in BOOK.get_items():
+    if item.get_type() == ebooklib.ITEM_DOCUMENT:
+        STRIPPED_BOOK = remove_tags(item.get_content())
+
+print(STRIPPED_BOOK)
